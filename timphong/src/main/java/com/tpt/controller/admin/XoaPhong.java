60dcaf84b3bb957a53404300934a4351e350b823
LamMN1012 <01274757673lam@gmail.com>
2022-12-05 21:43:32 +0700

    first commit

---
+++ timphong/src/main/java/com/tpt/controller/admin/XoaPhong.java
@@ -0,0 +1,47 @@
+package com.tpt.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.tpt.service.IPhongService;
+import com.tpt.service.impl.PhongServiceImpl;
+
+@WebServlet(urlPatterns = {"/admin/xoa-phong"})
+public class XoaPhong extends HttpServlet
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		String id_pString = req.getParameter("id_p");
+		int id_p;
+		IPhongService phongService = new PhongServiceImpl();
+		if(id_pString != null)
+		{
+			id_p = Integer.parseInt(id_pString);
+			phongService.deletePhong(id_p);
+			resp.sendRedirect(req.getContextPath() + "/admin/list-phong");
+		}
+		String id_taikhoan  = req.getParameter("id_taikhoan");
+		String id_loaip = req.getParameter("id_loaip");
+		if(id_taikhoan != null)
+		{
+			resp.sendRedirect(req.getContextPath() + "/admin/taikhoan?id_tk=" + id_taikhoan);
+		}
+		if(id_loaip != null)
+		{
+			resp.sendRedirect(req.getContextPath() + "/admin/loaiphong?id_lp=" + id_loaip);
+		}
+	}
+}
---
+++ timphong/src/main/java/com/tpt/dao/IDathenDao.java
@@ -0,0 +1,25 @@
+package com.tpt.dao;
+
+import java.util.List;
+
+import com.tpt.model.Dathen;
+
+public interface IDathenDao
+{
+
+	boolean editDathen(Dathen dathen);
+
+	boolean deleteDathen(int id_dh, int id_tk, int id_p);
+
+	boolean insertDathen(Dathen dathen);
+
+	List<Dathen> findByTrangthai(int id_tk, int tt);
+
+	Dathen findDathen(int id_dh, int id_tk, int id_p);
+
+	List<Dathen> findBySeller(int id_tk, int tt);
+
+	List<Dathen> findAll(int tt);
+	
+	
+}
